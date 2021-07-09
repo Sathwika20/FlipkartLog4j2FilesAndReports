8db371ba0990a90a8e3fe52707e89c471cd72d49
Sathwika20 <gopirajsathwika1999@gmail.com>
2021-07-09 15:51:07 +0530

    Generated Allure reports

--- src/main/java/com/bridgelabz/pages/Login.java
+++ src/main/java/com/bridgelabz/pages/Login.java
@@ -30,5 +30,6 @@ public class Login extends Base {
         loginButton.click();
         Thread.sleep(2000);
         log.info("flipakrtLoginTest");
+
     }
 }
--- src/test/java/com/bridgelabz/test/FlipkartTest.java
+++ src/test/java/com/bridgelabz/test/FlipkartTest.java
@@ -7,17 +7,25 @@ import com.bridgelabz.pages.Logout;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import jdk.jfr.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 @Listeners(CustomListeners.class)
 public class FlipkartTest extends Base {
+    public static WebElement webElement = driver.findElement(By.xpath("//div[@class ='exehdJ']"));
+    public static String elementValue = webElement.getText();
+
     @Description("Flipkart Login Test With Valid Credentials")
     @Severity(SeverityLevel.CRITICAL)
     @Test(priority = 1)
     public void LoginTest() throws InterruptedException {
         Login login = new Login(driver);
         login.getUserName();
+        System.out.println(elementValue);
+        Assert.assertEquals(elementValue,"Sathwika");
     }
     @Description("Flipkart Logout Test From The User Account")
     @Severity(SeverityLevel.NORMAL)
@@ -25,5 +33,7 @@ public class FlipkartTest extends Base {
     public static void logout() throws InterruptedException {
         Logout logout = new Logout(driver);
         logout.logout();
+        System.out.println(elementValue);
+        Assert.assertNotEquals(elementValue,"Sathwika");
     }
 }
